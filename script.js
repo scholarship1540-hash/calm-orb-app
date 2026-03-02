38c220c9bfb213f10e1b1aa7b08122592dd3f463
Akash S <scholarship1540@gmail.com>
2026-03-02 18:41:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-document.addEventListener("DOMContentLoaded", async function () {
-
-  const video = document.getElementById("camera");
-  const instruction = document.getElementById("instruction");
-
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: true
-    });
-
-    video.srcObject = stream;
-    instruction.innerText = "Camera is working";
-
-  } catch (err) {
-    instruction.innerText = "Camera error: " + err.message;
-    console.error(err);
-  }
-
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("instruction").innerText = "SCRIPT IS RUNNING";
 });
